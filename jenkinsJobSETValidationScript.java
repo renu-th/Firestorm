031561c71d4a6e15c79c47886954464bb4c2c601
renu-th <renu.thorati@gmail.com>
2019-06-27 00:12:22 -0700

    Update jenkinsJobSETValidationScript.java

--- jenkinsJobSETValidationScript.java
+++ jenkinsJobSETValidationScript.java
@@ -1,16 +1,3 @@
-/*************************************************************************
- * ADOBE CONFIDENTIAL ___________________
- * <p/>
- * Copyright 2017 Adobe Systems Incorporated All Rights Reserved.
- * <p/>
- * NOTICE: All information contained herein is, and remains the property of Adobe Systems
- * Incorporated and its suppliers, if any. The intellectual and technical concepts contained herein
- * are proprietary to Adobe Systems Incorporated and its suppliers and are protected by all
- * applicable intellectual property laws, including trade secret and copyright laws. Dissemination
- * of this information or reproduction of this material is strictly forbidden unless prior written
- * permission is obtained from Adobe Systems Incorporated.
- **************************************************************************/
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
