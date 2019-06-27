b76749e56081e376c3156e3c5319343fd6c7a616
renu-th <renu.thorati@gmail.com>
2019-06-27 00:12:06 -0700

    Update jenkinsJobSETValidationScript.java

--- jenkinsJobSETValidationScript.java
+++ jenkinsJobSETValidationScript.java
@@ -1,3 +1,16 @@
+/*************************************************************************
+ * ADOBE CONFIDENTIAL ___________________
+ * <p/>
+ * Copyright 2017 Adobe Systems Incorporated All Rights Reserved.
+ * <p/>
+ * NOTICE: All information contained herein is, and remains the property of Adobe Systems
+ * Incorporated and its suppliers, if any. The intellectual and technical concepts contained herein
+ * are proprietary to Adobe Systems Incorporated and its suppliers and are protected by all
+ * applicable intellectual property laws, including trade secret and copyright laws. Dissemination
+ * of this information or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from Adobe Systems Incorporated.
+ **************************************************************************/
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,23 +25,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author tat13876 (Renuka Thorati) DB Setup:
- *         https://wiki.corp.adobe.com/pages/viewpage.action?pageId=1270022214
- *         GIT Repo: https://git.corp.adobe.com/Ecommerce/OrderCaptureScripts
- *         JIRA Issue: https://jira.corp.adobe.com/browse/ECOMM-73133
+ * Generates Insert Scripts for STE DB - MySQL & Cassandra Edu Validation DB
+ * 
+ * @author tat13876 (Renuka) 
+ * @version 1.0
+ * @since   2019-06-26 
+ * GIT Repo: https://git.corp.adobe.com/Ecommerce/OrderCaptureScripts
+ * JIRA Issue: https://jira.corp.adobe.com/browse/ECOMM-73133
  */
 public class jenkinsJobSETValidationScript {
 
 	private static final String DELIMITER = ",";
-	//public final static String csvFile = "C:\\Adobe\\CacheBack\\DBScripts\\Batch2BadData\\batch2Data.csv";
 	private static final String SQL_OUTFILE = "\\sqlInsertScripts.sql";
 	private static final String CQL_OUTFILE = "\\cqlInsertScripts.txt";
 	private final static String sqlTemplate = "INSERT INTO EduValidation.EDU_VALIDATIONS(ID,USER_TYPE,COUNTRY,AREA_OF_STUDY,GRADUATION_YEAR,GRADUATION_MONTH,STATUS,EMAIL_ID,PERSON_ID) VALUES ('%s','%s','%s','%s',%s,%s,'%s','%s','%s');";
 	private final static String cqlTemplate = "INSERT INTO edu_validations.edu_validations(id,user_type,country,area_of_study,graduation_year,graduation_month,status,email,person_id,externally_verified,documents_uploaded,vendor_approved_status) VALUES ('%s','%s','%s','%s',%s,%s,'%s','%s','%s',%s,%s,%s);";
-	private static String jenkinsWorkspace;
-	/**
-	 * Code to generate Insert Scripts for STE DB - MySQL & Cassandra Scripts
-	 */
+	private static String jenkinsWorkspace; 
  
 	public static int generaterandomID() {
 		int randNum = 0;
@@ -45,51 +57,35 @@ public class jenkinsJobSETValidationScript {
 		}
 
 	}
-
-	public static void writeStringToFile(StringBuilder stmt, String fileName) {
-		File file = new File(fileName);
-		try {
-			FileWriter fw = new FileWriter(file);
-			fw.write(stmt.toString());
-			fw.close();
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-
-	}
-
+	
 	public static void main(String[] args) {
 		List<String> sqlStatements = new ArrayList<>();
 		List<String> cqlStatements = new ArrayList<>();		
 		String csvFile = System.getenv("DATA_FILE"); // Jenkins Build Parameter - Input CSV File
-		String email = System.getenv("EMAIL"); // Jenkins Build Parameter
-		jenkinsWorkspace = System.getenv("WORKSPACE");
+		String email = System.getenv("EMAIL"); // Jenkins Build Parameter - Email
+		jenkinsWorkspace = System.getenv("WORKSPACE"); //Jenkins Current Workspace
 		if(!csvFile.isEmpty())
-		{
-		
-		int startID = generaterandomID();
-		try {
-			FileReader reader = new FileReader(jenkinsWorkspace+"\\"+csvFile);
-			BufferedReader br = new BufferedReader(reader);
-			String line = br.readLine();
-			int noOfLines = 0;
-			int count = 0;
-			while ((line = br.readLine()) != null) {
-				String[] values = line.split(DELIMITER);
-				startID++;
-				noOfLines++;
-				System.out.println(startID+"-D"+values[10].toUpperCase()+ values[8].substring(0, 2).toUpperCase()+values[11].toUpperCase()+values[12]+values[13]+values[9].toUpperCase()+email+values[0]);
-				sqlStatements.add(String.format(sqlTemplate,startID+"-D",values[10].toUpperCase(), values[8].substring(0, 2).toUpperCase(), values[11].toUpperCase(),values[12], values[13], values[9].toUpperCase(),email, values[0]));
-				cqlStatements.add(String.format(cqlTemplate,startID+"-D",values[10].toUpperCase(), values[8].substring(0, 2).toUpperCase(),values[11].toUpperCase(),values[12], values[13], values[9].toUpperCase(),email, values[0], false, false, false));	
+		{		
+			int startID = generaterandomID();
+			try {
+					FileReader reader = new FileReader(jenkinsWorkspace+"\\"+csvFile);
+					BufferedReader br = new BufferedReader(reader);
+					String line = br.readLine();
+					int noOfLines = 0;
+					while ((line = br.readLine()) != null) {
+						String[] values = line.split(DELIMITER);
+						startID++; noOfLines++;
+						sqlStatements.add(String.format(sqlTemplate,startID+"-D",values[10].toUpperCase(), values[8].substring(0, 2).toUpperCase(), values[11].toUpperCase(),values[12], values[13], values[9].toUpperCase(),email, values[0]));
+						cqlStatements.add(String.format(cqlTemplate,startID+"-D",values[10].toUpperCase(), values[8].substring(0, 2).toUpperCase(),values[11].toUpperCase(),values[12], values[13], values[9].toUpperCase(),email, values[0], false, false, false));	
+					}
+					writeToFile(sqlStatements, SQL_OUTFILE); // SQL Scripts
+					writeToFile(cqlStatements, CQL_OUTFILE); // Cassandra Scripts
+					System.out.print("Generated " + noOfLines + " SQL and CQL Insert Scripts.");
+			} catch (FileNotFoundException e) {
+				System.out.println(e.getMessage());
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
 			}
-			writeToFile(sqlStatements, SQL_OUTFILE); // SQL Scripts
-			writeToFile(cqlStatements, CQL_OUTFILE); // Cassandra Scripts
-			System.out.print("Generated " + noOfLines + " Insert Scripts.");
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
 		}
 		else
 			System.out.println("No File Found");
