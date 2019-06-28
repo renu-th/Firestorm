6f7006370a2fe5cccda9dedb567b13bef9488531
renu-th <renu.thorati@gmail.com>
2019-06-28 15:02:49 -0700

    Update STEValidation.java

--- STEValidation.java
+++ STEValidation.java
@@ -34,14 +34,13 @@ import java.util.List;
 public class STEValidation {
 
     private static final String DELIMITER = ",";
-    private static final String SQL_OUTFILE = "\\sqlInsertScripts.sql";
-    private static final String CQL_OUTFILE = "\\cqlInsertScripts.txt";
     private static final String SQL_TEMPLATE = "INSERT INTO EduValidation.EDU_VALIDATIONS(ID,USER_TYPE,COUNTRY,AREA_OF_STUDY,GRADUATION_YEAR,GRADUATION_MONTH,STATUS,EMAIL_ID,PERSON_ID) VALUES ('%s','%s','%s','%s',%s,%s,'%s','%s','%s');";
     private static final String CQL_TEMPLATE = "INSERT INTO edu_validations.edu_validations(id,user_type,country,area_of_study,graduation_year,graduation_month,status,email,person_id,externally_verified,documents_uploaded,vendor_approved_status) VALUES ('%s','%s','%s','%s',%s,%s,'%s','%s','%s',%s,%s,%s);";
     private static final String BOOL_VALUE = "False";
 
     private static String inFilePath;
-    private static String outFilePath;
+    private static String sqlFilePath;
+    private static String cqlFilePath;
     private static String email;
     private static BufferedReader br;
 
@@ -52,7 +51,7 @@ public class STEValidation {
     }
 
     public static void writeToFile(List<String> sqlStatements, String fileName) {
-        Path fileP = Paths.get(outFilePath);
+        Path fileP = Paths.get(fileName);
         try {
             Files.write(fileP, sqlStatements, StandardCharsets.UTF_8);
         } catch (IOException e) {
@@ -60,50 +59,52 @@ public class STEValidation {
         }
     }
 
-    public static void fetchCommandLineArgs(String[] cmdLinArgs) {
+    public static boolean fetchCommandLineArgs(String[] cmdLinArgs) {
         if (cmdLinArgs.length > 0 && cmdLinArgs.length == 3) {
             email = cmdLinArgs[0];
             inFilePath = cmdLinArgs[1];
-            outFilePath = cmdLinArgs[2];
-            System.out.println(inFilePath);
-            System.out.println(outFilePath);
+            sqlFilePath = cmdLinArgs[2];
+            cqlFilePath = cmdLinArgs[2];
         } else {
             System.out.println("Not enough Command Line Arguments being passed.");
+            return false;
         }
-
+        return true;
     }
 
     public static void main(String[] args) {
         List<String> sqlStatements = new ArrayList<String>();
         List<String> cqlStatements = new ArrayList<String>();
 
-        fetchCommandLineArgs(args);
+        Boolean isValidArgs = false;
+        isValidArgs = fetchCommandLineArgs(args);
 
-        try {
-            FileReader reader = new FileReader(inFilePath);
-            br = new BufferedReader(reader);
-            String line = br.readLine();
-            int noOfLines = 0;
-            while ((line = br.readLine()) != null) {
-                String id = generateId();
-                String[] values = line.split(DELIMITER);
-                noOfLines++;
-                sqlStatements.add(String.format(SQL_TEMPLATE, id, values[10].toUpperCase(), values[8].substring(0, 2).toUpperCase(),
-                        values[11].toUpperCase(), values[12], values[13], values[9].toUpperCase(), email, values[0]));
-                cqlStatements.add(String.format(CQL_TEMPLATE, id, values[10].toUpperCase(), values[8].substring(0, 2).toUpperCase(),
-                        values[11].toUpperCase(), values[12], values[13], values[9].toUpperCase(), email, values[0], BOOL_VALUE, BOOL_VALUE,
-                        BOOL_VALUE));
+        if (isValidArgs) {
+            try {
+                FileReader reader = new FileReader(inFilePath);
+                br = new BufferedReader(reader);
+                String line = br.readLine();
+                int noOfLines = 0;
+                while ((line = br.readLine()) != null) {
+                    String id = generateId();
+                    String[] values = line.split(DELIMITER);
+                    noOfLines++;
+                    sqlStatements.add(String.format(SQL_TEMPLATE, id, values[10].toUpperCase(), values[8].substring(0, 2).toUpperCase(),
+                            values[11].toUpperCase(), values[12], values[13], values[9].toUpperCase(), email, values[0]));
+                    cqlStatements.add(String.format(CQL_TEMPLATE, id, values[10].toUpperCase(), values[8].substring(0, 2).toUpperCase(),
+                            values[11].toUpperCase(), values[12], values[13], values[9].toUpperCase(), email, values[0], BOOL_VALUE, BOOL_VALUE,
+                            BOOL_VALUE));
+                }
+
+                writeToFile(sqlStatements, sqlFilePath);
+                writeToFile(cqlStatements, cqlFilePath);
+
+                System.out.println("Generated " + noOfLines + " SQL and CQL Insert Scripts.");
+            } catch (FileNotFoundException e) {
+                System.out.println(e.getMessage());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
             }
-
-            writeToFile(sqlStatements, SQL_OUTFILE);
-            writeToFile(cqlStatements, CQL_OUTFILE);
-
-            System.out.println("Generated " + noOfLines + " SQL and CQL Insert Scripts.");
-
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
         }
     }
 
